abb9ca24738e147162b33963a602a3eb19fb98cf
Xiaochun Mou <xiaochun_mou@epcnchew0384.epam.com>
2024-12-16 11:45:36 +0800

    updated HomePage

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,10 +1,25 @@
-function HomePage() {
- 
+import React, { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const HomePage = () => {
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+  
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      // 如果用户未登录，重定向到登录页面
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, isLoading, loginWithRedirect]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div >
-       Home Page
+    <div>
+      Welcome to the home page!
     </div>
   );
-}
+};
 
 export default HomePage;
