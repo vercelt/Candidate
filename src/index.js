4a077852b7634b3bfcf56baeab542477f17d8ceb
Xiaochun Mou <xiaochun_mou@epcnchew0384.epam.com>
2024-12-17 17:47:18 +0800

    updated redicturi

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={`${window.location.origin}`}
+      redirectUri={`${window.location.origin + '/home'}`}
     >
       <App />
     </Auth0Provider>
