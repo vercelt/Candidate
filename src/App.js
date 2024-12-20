1004016741f7f92f1277bf64f8bcf31477d42a38
Xiaochun Mou <xiaochun_mou@epcnchew0384.epam.com>
2024-12-20 10:01:30 +0800

    Updated side bar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Dashboard from './home/Dashboard.js';
 import { useAuth0 } from "@auth0/auth0-react";
-import {Route ,Routes } from 'react-router-dom';
+import {Route ,Routes, Navigate } from 'react-router-dom';
 import Loading from './load/LoadingPage.js';
 import UserProfile from './home/profile/UserProfile';
 import UserSettings from './home/settings/UserSettings';
-import Sidebar from './home/sidebar/Sidebar'
-
+import Overview from './home/overview/Overview.js';
 function App() {
   const { isAuthenticated } = useAuth0();
   console.log('isAuthenticated', isAuthenticated)
@@ -15,14 +14,15 @@ function App() {
       <Routes>
       { isAuthenticated && (
         <Route path="/home" element={<Dashboard />} >
+          <Route path="/home/overview" element={<Overview />} />
           <Route path="/home/profile" element={<UserProfile />} />
           <Route path="/home/settings" element={<UserSettings />} />
+          <Route index element={<Navigate to="/home/overview" replace />} />
         </Route>
       )}
       <Route path="/" element={<Loading />} />
     </Routes>
     </div>
-   
   );
 }
 
---
+++ src/home/overview/Overview.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Overview = () => {
+    return <div>Welcome to the user overview Page!</div>;
+  };
+  
+  export default Overview;
